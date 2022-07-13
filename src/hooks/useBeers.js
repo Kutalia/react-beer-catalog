89891be7b796561643fe76dd47cbf858d7511985
Kote Kutalia <kotekutalia@gmail.com>
2022-07-13 15:59:11 +0400

    rewrote beers fetching hook to fetch minimum required amount of unique beers

--- src/hooks/useBeers.js
+++ src/hooks/useBeers.js
@@ -1,55 +1,76 @@
-import { useEffect, useState, useMemo } from 'react';
+import { useEffect, useState } from 'react';
 
 import dummyBeer from './dummyData.json';
 
 const useDummyData = process.env.REACT_APP_DUMMY_DATA === "true";
+const endpoint = 'https://api.punkapi.com/v2/beers/random';
+const statuses = {
+  ALL_LOADED: 'all_loaded',
+  LOADED: 'loaded',
+  ERROR: 'error',
+};
+
+const fetchBeer = async (cache, limit) => {
+  try {
+    const response = await fetch(endpoint);
+    const data = (await response.json())[0];
+
+    if (!cache[data.id]) {
+      cache[data.id] = data;
+
+      if (Object.values(cache).length === limit) {
+        console.log('FINISHED LOADING ALL BEERS', cache);
+        return {
+          status: statuses.ALL_LOADED,
+          data: cache,
+        };
+      } else {
+        console.log('BEER SUCCESSFULLY LOADED', data)
+        return {
+          status: statuses.LOADED,
+          data,
+        };
+      }
+    } else {
+      console.warn('NON UNIQUE BEER DETECTED!', data);
+      return (await fetchBeer(cache, limit)) ;
+    }
+  } catch (error) {
+    console.error('ERROR LOADING BEER');
+    return {
+      status: statuses.ERROR,
+      data: error,
+    };
+  }
+}
 
 const useBeers = (limit) => {
-  const [beers, setBeers] = useState({});
+  const [beers, setBeers] = useState([]);
 
   useEffect(() => {
     const cache = {};
 
     let i = 0;
-    let loadedItemsCount = 0;
-    // pulling 30 potentially non-unique beers without blocking
-    // if we want to guarantee uniqueness, async IIFE function in incrementing loop could be used
+
     for (i; i < limit; i++) {
       if (useDummyData) {
         const id = dummyBeer.id + i;
         cache[id] = { ...dummyBeer, id };
       } else {
-        fetch('https://api.punkapi.com/v2/beers/random')
-          // using "unsafe" variable is a desired effect
-          /*eslint no-loop-func: "off"*/
-          .then(response => response.json())
-          .then((resp) => {
-            const data = resp[0];
-            loadedItemsCount++;
-
-            if (!cache[data.id]) {
-              cache[data.id] = data;
-            }
-
-            // approached limit
-            // better than calling Object.values().length evey time
-            if (loadedItemsCount === 30) {
-              setBeers(cache);
-            }
+        fetchBeer(cache, limit).then(({ status, data }) => {
+          if (status === statuses.ALL_LOADED) {
+            setBeers(Object.values(data));
           }
-          );
+        });
       }
     }
 
     if (useDummyData) {
-      setBeers(cache);
+      setBeers(Object.values(cache));
     }
   }, [limit]);
 
-  // useMemo compares object reference, so it will work correctly on each `beers` update
-  const beersArr = useMemo(() => Object.values(beers), [beers]);
-
-  return beersArr;
+  return beers;
 };
 
 export default useBeers;
